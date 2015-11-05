8a11c104d1782e728508393885a5d89210427123
Zack Coker <zack_coker@yahoo.com>
2015-11-05 11:45:42 -0500

    updated the code so the violation is in all fragments

--- Application/src/main/java/com/example/android/lnotifications/HeadsUpNotificationFragment.java
+++ Application/src/main/java/com/example/android/lnotifications/HeadsUpNotificationFragment.java
@@ -80,7 +80,7 @@ public class HeadsUpNotificationFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_heads_up_notification, container, false);
+        return inflater.inflate(R.layout.fragment_heads_up_notification, container);
     }
 
     @Override
--- Application/src/main/java/com/example/android/lnotifications/VisibilityMetadataFragment.java
+++ Application/src/main/java/com/example/android/lnotifications/VisibilityMetadataFragment.java
@@ -82,7 +82,7 @@ public class VisibilityMetadataFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_visibility_metadata_notification, container, false);
+        return inflater.inflate(R.layout.fragment_visibility_metadata_notification, container);
     }
 
     @Override
