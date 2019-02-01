989c5222df4d3218e505aac6e5286c1c9983150c
ZackC <zack_coker@yahoo.com>
2019-01-31 23:27:10 -0500

    latest violation

--- Application/src/main/java/com/example/android/lnotifications/LNotificationActivity.java
+++ Application/src/main/java/com/example/android/lnotifications/LNotificationActivity.java
@@ -20,7 +20,15 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Random;
+
+//Possible Stackoverflow question of interest (example of real world issue):
+//https://stackoverflow.com/questions/13345656/settheme-function-not-working-on-resume
+
 
 /**
  * Launcher Activity for the L Notification samples application.
@@ -29,8 +37,25 @@ public class LNotificationActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //setTheme(android.R.style.Theme_DeviceDefault_Light);
+        //if(this.getTheme() == android.R.style.ThemeOverlay_Material_Dark){
+
+        //}
+        Log.i("whatever", "changing the theme");
+        Random rand = new Random();
+        int choice = rand.nextInt(2);
+        int theme = 0;
+        Log.i("whatever", "choosing theme: "+choice);
+        if (choice == 0) {
+            theme = android.R.style.ThemeOverlay_Material_Dark;
+        }
+        else if (choice == 1) {
+            theme = android.R.style.Theme_DeviceDefault_Light;
+        }
         super.onCreate(savedInstanceState);
+        setTheme(theme);
         setContentView(R.layout.activity_notification);
+        //setTheme(theme);
         setTitle(R.string.title_lnotification_activity);
         ActionBar actionBar = getActionBar();
 
@@ -50,6 +75,23 @@ public class LNotificationActivity extends Activity {
         actionBar.addTab(tabHeadsUpNotification, 0);
         actionBar.addTab(tabVisibilityMetadata, 1);
         actionBar.addTab(tabOtherMetadata, 2);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.i("whatever", "changing the theme in onResume");
+        Random rand = new Random();
+        int choice = rand.nextInt(2);
+        int theme = 0;
+        Log.i("whatever", "choosing theme: "+choice);
+        if (choice == 0) {
+            theme = android.R.style.ThemeOverlay_Material_Dark;
+        }
+        else if (choice == 1) {
+            theme = android.R.style.Theme_DeviceDefault_Light;
+        }
+        setTheme(theme);
     }
 
     /**
