e5820cff74c52a387a0dda1b1cd8784afab99e26
ZackC <zack_coker@yahoo.com>
2019-03-06 10:26:48 -0500

    adding change - might not be finished but want to change to a new branch

--- Application/src/main/java/com/example/android/lnotifications/BackgroundTestFragment.java
+++ Application/src/main/java/com/example/android/lnotifications/BackgroundTestFragment.java
@@ -7,14 +7,29 @@ import android.util.Log;
 
 //problem taken from https://stackoverflow.com/questions/47723082/fragment-illegalstatexception-on-getresources
 
+//might later need to expand either this example or the checker to handle other ways the Fragment can
+//be in the background (https://developer.android.com/reference/android/os/AsyncTask) AsyncTasks should
+// ideally be used for short operations (a few seconds at the most.) If you need to keep threads running
+// for long periods of time, it is highly recommended you use the various APIs provided by the
+// java.util.concurrent package such as Executor, ThreadPoolExecutor and FutureTask.
+
+//the question and answer in this case is vague enough that I'm not sure what the correct answer
+//should be for this example
+
 public class BackgroundTestFragment extends Fragment {
-    private class ResourcesTask extends AsyncTask{
+    private class ResourcesTask extends AsyncTask<Object, Object, String>{
 
         @Override
-        protected Object doInBackground(Object[] objects) {
+        protected String doInBackground(Object... voids) {
             Log.i("Background Info","in doInBackground of ResourcesTask");
+            //BackgroundTestFragment.this.getResources();
+            return "";
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            Log.i("Background Info","in onPostExecute of ResourcesTask");
             BackgroundTestFragment.this.getResources();
-            return new Object();
         }
     }
 
